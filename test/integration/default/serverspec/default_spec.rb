69f12ca30c599b09b6c5d37705f2d408b05c92c2
Alex Knoll <arknoll@gmail.com>
2014-02-22 17:21:04 -0700

    update serverspec test

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,8 +1,6 @@
 require "spec_helper"
 
 # Write integration tests with Serverspec - http://serverspec.org/
-describe "drupal-solr::default" do
-  describe port(8080) do
-    it { should be_listening }
-  end
+describe port(8080) do
+  it { should be_listening }
 end
